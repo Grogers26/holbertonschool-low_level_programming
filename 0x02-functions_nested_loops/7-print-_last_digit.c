244f53ef2db29ec7e1d2c2a365e1af2c5b504684
grogers26 <grogers26@outlook.com>
2023-01-30 11:30:37 -0800

    $

---
+++ 0x02-functions_nested_loops/7-print-_last_digit.c
@@ -0,0 +1,22 @@
+#!/bin/bash
+#include "main.h"
+
+/**
+ * print_last_digit - Function that prints the last digit of a number
+ *
+ * @n: Number we are printing the last digit of.
+ *
+ * Return: The last digit.
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	if (n < 0)
+		n = -n;
+
+	_putchar(n + '0');
+
+	return (n);
+}
