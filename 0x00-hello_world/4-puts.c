f0e279315fca6432899d11a8f31da0b195d3b330
grogers26 <grogers26@outlook.com>
2023-01-30 08:14:38 -0800

    $

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+/*
+ * File: 4-puts.c
+ * Auth: George Rogers <6148@holbertonschool.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
