9e09b9ffc963553dd37a0dcb537efc9ca2ce75a9
George Rogers <grogers26@outlook.com>
2023-02-10 13:24:16 -0800

    holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,24 +1,14 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/**
- * File: holberton.h
- * 
- *  Header file containing prototypes for all functions
- *       used in the 0x06-pointers_arrays_strings directory.
- */
-
-#include <stddef.h>
+#ifndef holberton.h
+#define holberton.h
 
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
-
-#endif
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
