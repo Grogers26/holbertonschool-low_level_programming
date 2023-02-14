2843f5c20d90aa5c77bba7d0fcf5d113acf935d8
George Rogers <grogers26@outlook.com>
2023-02-13 18:01:53 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[b][c]);
+		}
+
+		_putchar('\n');
+	}
+}
