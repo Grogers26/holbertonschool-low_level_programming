9fb166a3ff91da719f78e0b8de06a24c39d5377d
George Rogers <grogers26@outlook.com>
2023-02-13 17:28:33 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+char *_strchr(char *s, char c)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+
+	return ('\0');
+}
