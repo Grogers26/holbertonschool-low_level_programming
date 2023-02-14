78f7f41219337e638517b2a88bb3d964cd5ac4cc
George Rogers <grogers26@outlook.com>
2023-02-13 17:17:39 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  * _memset - Fill memory with a constant byte
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+
+	return (s);
+}
