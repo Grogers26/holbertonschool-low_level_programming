365fdb4751f741343580bc985f03163451ceeed9
George Rogers <grogers26@outlook.com>
2023-02-13 17:41:46 -0800

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+char *_strpbrk(char *s, char *accept)
+{
+  int a = 0, b;
+
+  while (s[a])
+    {
+      b = 0;
+
+      while (accept[b])
+	{
+	  if (s[a] == accept[b])
+	    {
+	      s += a;
+	      return (s);
+	    }
+
+	  b++;
+	}
+
+      a++;
+    }
+
+  return ('\0');
+}~
