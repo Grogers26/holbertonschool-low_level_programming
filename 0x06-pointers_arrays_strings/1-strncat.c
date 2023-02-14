da4152202c43adbbcfebfa1033b05bec51d26a39
George Rogers <grogers26@outlook.com>
2023-02-13 19:33:25 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,4 @@
-#include "holberton.h"
-
-/**
-  * _strncat - Concatenates two strings
-  * @dest: The destination value
-  * @src: The source value
-  * @n: The limit of the concatenation
-  *
-  * Return: A pointer to the resulting string dest
-  */
+#include "main.c"
 char *_strncat(char *dest, char *src, int n)
 {
 	int dlen = 0, j = 0;
