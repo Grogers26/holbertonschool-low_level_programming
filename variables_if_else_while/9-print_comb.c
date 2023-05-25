679add8620711ce2ccc38ee43b607aaa9ba4d12a
Grogers26 <grogers26@outlook.com>
2023-05-25 11:43:04 -0700

    getting better

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,20 +5,18 @@
  * Return: Always 0.
  */
 int main(void)
-
 {
-  int num;
-  for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
+ int num;
 
-		continue;
-		putchar(',');
-	putchar(' ');
-}
-}
-  putchar('\n');
+ for (num = 0; num <= 9; num++)
+{
+ putchar((num % 10) + '0');
+ if (num == 9)
+   continue;
+ putchar(',');
+ putchar(' ');
+ }
+ putchar('\n');
 
-return (0);
+ return (0);
 }
