c23340027e45d0ddabc0c61d0f9d7d9045b70ac2
Grogers26 <grogers26@outlook.com>
2023-05-24 17:19:29 -0700

    getting better

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/*
+/**
+*
 *
 *main - prints a sentence using the puts command
-*Return: main returns 0
+*Return: always 0
 */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+     puts ("\"Programming is like building a multilingual puzzle/n");
+     return (0);
 }
