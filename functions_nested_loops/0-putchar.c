ccc857b9070f21729bae01d6d629c292bf94926a
Grogers26 <grogers26@outlook.com>
2023-05-30 20:07:31 -0700

    progress 3

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,18 +1,12 @@
 #include<stdio.h>
 /**
- * main - Function that prints the string "_putchar"
+ * main - Function _putchar"
  *
  * Return: Function _putchar.c that prints a newline character.
  */
-int main(void)
+int main()
 {
-int pp[9] = { '_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n' };
-int i = 0;
- while (i < 9);
-{
-  printf(_putchar(pp[i]);
-	(i++);
-}
+  printf("_putchar\n");
 
 	return (0);
 }
