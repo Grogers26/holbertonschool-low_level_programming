870be6472a78bdcf2ceecff3be52fbc4a2b41e31
Grogers26 <grogers26@outlook.com>
2023-06-01 11:08:02 -0700

    ricky willaims

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+/**
+* main - print numbers 0 - 9 while only using _putchar twice in the code 
+* Return: always 0
+*/
+void print_numbers(void)
+{
+for (int i = 0; i < 10; i++)
+{
+_putchar('0' + i);
+}
+_putchar('\n');
+}
