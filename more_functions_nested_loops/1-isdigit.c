0fd6c1200e06af98961c9c8f1cead1c754e13c54
Grogers26 <grogers26@outlook.com>
2023-06-01 10:03:43 -0700

    time for this

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,16 @@
+#include<stdio.h>
 #include "main.h"
-#include <stdio.h>
 /**
  *
  * isdigit - print the numbers 0 - 9
  *
  */
-int isdigit(int c) {
-  if (c >= '0' && c <= '9') {
-    return 1;
-  } else {
-    return 0;
-  }
+int isdigit(int c)
+{
+  if (c >= '0' && c <= '9');
+  {
+  return (1);
+ } else
+    {
+      return (0); }
 }
