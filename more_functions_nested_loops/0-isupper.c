a2ad5d0436b4ef451d0e48ade8a6847a7b449fb8
Grogers26 <grogers26@outlook.com>
2023-05-31 17:31:50 -0700

    pushing to the luck1

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,10 @@
-#include<main.h>
+#include "main.h"
+#include <stdio.h>
 /**
  *main - checking for uppercase letter. using isupper and return.
  *
  */
-int isupper(char ch)
+int isupper(int ch)
 {
 return (ch >= 'A' && ch <= 'Z');
 }
