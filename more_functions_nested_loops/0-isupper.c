b8d3f3df7f88b3c7acaa773e90d5cb0a3beeea9a
Grogers26 <grogers26@outlook.com>
2023-06-01 09:38:13 -0700

    ugh

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,10 @@
  *_isupper - checking for uppercase letter. using isupper and return.
  *
  */
-int _isupper(int c)
-{
-if (c >= 65 && c <= 90)
- {
-   return (1);
+int _isupper(int c){
+if (c >= 65 && c <= 90) {
+   return 1;
+ } else {
+return 0;
  }
- else
-   }
+}
