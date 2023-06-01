2cb5c83da040540efe09a50f356b80e0d6d0c345
Grogers26 <grogers26@outlook.com>
2023-06-01 11:16:20 -0700

    flags

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,9 @@
 * main - print numbers 0 - 9 while only using _putchar twice in the code 
 * Return: always void 
 */
-void print_numbers(void);
-{
-for (int i = 0; i < 10; i++)
-  {
+void print_numbers(void) {
+for (int i = 0; i < 10; i++) {
 _putchar('0' + i);
  }
-_putchar('\n');
+ _putchar('\n');
 }
