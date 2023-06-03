f58123ab26053c2d9108cce5f867c6dae0c7eb26
Grogers26 <grogers26@outlook.com>
2023-06-03 10:02:10 -0700

    lucky

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
+#include <stdio,h>
 #include "main.h"
 /**
 *main - print numbers 0 - 9 while only using _putchar twice in the code
 *
 *Return: always (void)
 */
-void print_numbers(void)
+int  main(void)
 {
-  for (int i = 0; i < 10; i++)
+  for(int i = 0; i < 10; i++)
     {
 _putchar('0' + i);
- _putchar(' ');
     }
   _putchar('\n');
     }
