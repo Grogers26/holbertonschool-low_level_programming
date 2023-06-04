26fd46ff5e5de3937e45f9f825d03edd5db33914
Grogers26 <grogers26@outlook.com>
2023-06-03 19:02:27 -0700

    closer

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * task - print numbers 0-9 but -c 2 and 4. while using _putchar only twice
+ *
+ *
+* return: void 
+ */
+void print_most_numbers(void)
+{
+  int i;
+    for (i = 0; i <= 9; i++)
+      {
+	if (i != 2 && i != 4) {
+	  _putchar(i + '0');
+	}
+      }
+  _putchar('\n');
+}
