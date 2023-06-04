d008201d1158d2a8e7093e1e5080f92ef93bda1b
Grogers26 <grogers26@outlook.com>
2023-06-03 19:57:28 -0700

    kids

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - printing number mulitple times
+ * 
+ * return: (0)
+ */
+void more_numbes(void)
+{
+  int i;
+  for (i = 0; i < 10; i++)
+    {
+      for (i = 0; i <= 14; i++)
+	{
+	  _putchar(i + '0');
+	}
+      _putchar('\n');
+    }
+}
