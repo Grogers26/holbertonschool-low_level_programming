1472f699679bcdbae1de10c1353f0d0bcb83f212
Grogers26 <grogers26@outlook.com>
2023-06-03 21:08:45 -0700

    openline

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - draw a straight line in the terminal 
+ *
+ * return: void
+ */
+void print_line(int n)
+{
+  if (n <= 0)
+    {
+      _putchar('\n');
+      return;
+    }
+  for (i = 0; i < n; i++)
+    {
+      _putchar('_');
+    }
+}
