ce1f2a53158b1616ea09c1eeb00b8974d73774c1
Grogers26 <grogers26@outlook.com>
2023-06-05 01:29:01 -0700

    maybe2

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,20 +7,22 @@
 int main(void)
 {
 	int i;
+	
 	i = 1;
+	
 	for (i = 1; i <= 100; i++)
 {
 	if ((i % 3 == 0) && (i % 5 == 0))
 	{
-		printf("FizzBuzz ");
+		printf("FizzBuzz");
 	}
 	if (i % 3 == 0) 
 	{
-		printf("Fizz ");
+		printf("Fizz");
 	}
 	if (i % 5 == 0)
 	{
-		printf("Buzz ");
+		printf("Buzz");
 	}
 	else
 	{
