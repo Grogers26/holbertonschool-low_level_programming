9605b0f1306f8ef91e9731334a38f9cbfef03be3
Grogers26 <grogers26@outlook.com>
2023-06-05 14:27:00 -0700

    grind

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,24 +7,27 @@
 int main(void)
 {
 	int i;
+	
 	for (i = 1; i <= 100; i++)
 {
 	if ((i % 3 == 0) && (i % 5 == 0))
 	{
-		printf("FizzBuzz ");
+		printf("FizzBuzz");
 	}
-	if (i % 3 == 0)
+	else if (i % 3 == 0)
 	{
-		printf("Fizz ");
+		printf("Fizz");
 	}
-	if (i % 5 == 0)
+	else if (i % 5 == 0)
 	{
-		printf("Buzz ");
+		printf("Buzz");
 	}
 	else
 	{
-		printf("%d ", i);
+		printf("%d", i);
 	}
+	if (i < 100)
+		printf(' ');
 }
 printf("\n");
 return (0);
