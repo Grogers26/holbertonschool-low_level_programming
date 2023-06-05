eecb4923300e790ff7a7c5bc5a1746109a1a0540
Grogers26 <grogers26@outlook.com>
2023-06-04 23:23:46 -0700

    switched2vi

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+* main - print line diagonally n times.
+*
+* return: no return.
+*/
+void print_diagonal(int n)
+{
+int i, j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar(92);
+if (i < (n - 1))
+_putchar('\n');
+}
+_putchar('\n');
+}
