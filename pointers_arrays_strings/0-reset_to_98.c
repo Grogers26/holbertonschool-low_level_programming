3db8e64d829a325d52cb37c8f48b0f3c697dc416
Grogers26 <grogers26@outlook.com>
2023-06-05 11:49:05 -0700

    never alone

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - take a pointer to a paramter and updates its value.
+ * @n: input integer.
+ * Return: no return.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
