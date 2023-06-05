3a6bb0b04253e90b317d8f44faab9f5ac0b692e9
Grogers26 <grogers26@outlook.com>
2023-06-05 14:40:01 -0700

    mario

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,15 +9,15 @@ void print_triangle(int size)
 	int i, j;
 	
 	if (size <= 0)
-		printf('\n');
+		printf("\n");
 	
 	for (i = 0; i <= size; i++)
 	{
 		for (j = 1; j <= (size - i); j++)
-			_putchar(' ');
+			_putchar(" ");
 		for (j--; j < size; j++)
 			_putchar(35);
 		if (i < (size - 1))
-			_putchar('\n');
+			_putchar("\n");
 	}
 }
