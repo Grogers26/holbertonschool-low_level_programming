07b1bbb93a5019814754592cbc5154c7fddd1d91
Grogers26 <grogers26@outlook.com>
2023-06-05 15:34:09 -0700

    nf

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-* print_line - draw a straight line in the terminal 
-* @c: printing lines 
+* print_line - draw a straight line in the terminal
+* @c: printing lines
 * return: void
 */
 void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++) 
+	for (i = 0; i <= n; i++)
 	{
 		_putchar(95);
 	}
