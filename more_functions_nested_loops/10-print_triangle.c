7f8f0d55e005b8a503443e70f4c5863e9c943d14
Grogers26 <grogers26@outlook.com>
2023-06-05 02:32:28 -0700

    dem

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print triangle - print a triangle
+ * @size: numbers of lines.
+ * Return: no return.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+}
