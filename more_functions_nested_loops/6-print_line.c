500b545a49bfb73a1840c1de9847d152d23e93f0
Grogers26 <grogers26@outlook.com>
2023-06-04 22:40:11 -0700

    behind

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - draw a straight line in the terminal 
- *
- * return: void
- */
+* main - draw a straight line in the terminal 
+*
+* return: void
+*/
 void print_line(int n)
 {
-  int i;
-  if (n <= 0)
-    {
-      _putchar('\n');
-      return;
-    }
-  for (i = 0; i < 10; i++)
-    {
+int i;
+ if (n <= 0)
+   {
+     _putchar('\n');
+     return;
+   }
+ for (i = 0; i <= 10; i++)
+   {
      _putchar('_');
-      return;
-    }
+     return;
+   }
 }
