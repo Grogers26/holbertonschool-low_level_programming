bf82ae2e83120a513464bd77f125fe0f40274e0f
Grogers26 <grogers26@outlook.com>
2023-06-05 16:34:12 -0700

    zero

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,12 +1,13 @@
 #include"main.h"
 /**
-* main - print line diagonally n times.
-* @n: the number of rows in a line 
+* print_diagonal - print line diagonally n times.
+* @n: the number of rows in a line
 * return: no return.
 */
 void print_diagonal(int n)
 {
 	int i, j;
+	
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
