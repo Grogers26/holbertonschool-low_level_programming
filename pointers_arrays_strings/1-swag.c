09d26834b49e0ba41e884aacd0f53d6d7adfda1d
Grogers26 <grogers26@outlook.com>
2023-06-05 12:41:19 -0700

    sky

---
+++ pointers_arrays_strings/1-swag.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swag_int - swap the values of two integers. 
+ * @a: first integer
+ * @b: second integer.
+ * Return: no return.
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
