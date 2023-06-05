46b9000ca0b4bb50bb398c07150d5bd0546c41ed
Grogers26 <grogers26@outlook.com>
2023-06-05 15:42:00 -0700

    gates

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -3,12 +3,12 @@
  * task - print numbers 0-9 but -c 2 and 4. while using _putchar only twice
  * @num: print most numbers
  *
- *
  * return: void
  */
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 
 	{
--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,7 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			_putchar((j / 10) +48);
+			_putchar((j / 10) + 48);
 			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
