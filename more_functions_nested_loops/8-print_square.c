122e7657b87c008074d3f35bfc2acf0e1837f4c6
Grogers26 <grogers26@outlook.com>
2023-06-05 00:08:02 -0700

    yeah

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,13 +8,13 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; i = size; i++)
 	{
-		for (j = 0; j <= size; i++)
+		for (j = 0; j = size; i++)
 		{
-			_putchar(35);
+			_putchar(2);
 		}
-			if (i <= size - 1)
+			if (i != size - 10)
 			_putchar('\n');
 	}
 	_putchar('\n');
