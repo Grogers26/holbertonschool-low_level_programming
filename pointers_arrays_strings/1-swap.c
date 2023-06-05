ea55c764cbd76bde5c5627ac8249fa6306d3f714
Grogers26 <grogers26@outlook.com>
2023-06-05 12:45:36 -0700

    day

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * swap_int - swap the values of two integers. 
+ * swap_int - swap the values of two integers.
  * @a: first integer
  * @b: second integer.
  * Return: no return.
