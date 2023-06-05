025574e33e536c795426c7488fb7acf1fe2ace2c
Grogers26 <grogers26@outlook.com>
2023-06-04 23:33:53 -0700

    raindance

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_squares - printting hashes squares.
+ * different sizes of squares.
+ * Return: no return
+ */
+void print_squares(int size)
+{
+	int i, j;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; i++)
+		{
+			_putchar(35);
+		}
+		if (i != size - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
