448bec8208bc8daf9901164cb785c04ad9089029
Grogers26 <grogers26@outlook.com>
2023-06-05 01:17:05 -0700

    trapped

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * fizzbuzz - counting to 100
+ * @num: counting
+ * return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+{
+	if (i % 3 == 0)  (i % 5 == 0);
+	{
+		printf("FizzBuzz ");
+	}
+	else (i % 3 == 0) 
+	{
+		printf("Fizz ");
+	}
+	else (i % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else 
+	{
+		printf("%d ", i);
+	}
+}
+printf("\n");
+return (0);
+}
