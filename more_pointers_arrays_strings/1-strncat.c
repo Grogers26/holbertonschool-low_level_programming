499c2c641feea65029ed2e6c3838f0326fcb852b
Grogers26 <grogers26@outlook.com>
2023-06-07 01:50:23 -0700

    brickhouse

---
+++ more_pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.c"
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlen = 0, j = 0;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	while (j < n && src[j])
+	{
+		dest[dlen] = src[j];
+		dlen++;
+		j++;
+	}
+
+	dest[dlen + n + 1] = '\0';
+
+	return (dest);
+}
+
