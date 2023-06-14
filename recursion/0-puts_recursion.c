b2d75807906701a02891a9660e9d50cfd0ad6a14
George Rogers <115730272+Grogers26@users.noreply.github.com>
2023-06-14 08:37:25 +0000

    forget me not

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * write a function that prints a string.
+ * @s: string
+ * return: no return  
+*/
+void _puts_recursion(char *s)
+{
+    if (*s != '\0')
+    {
+        putchar(*s);
+        _puts_recursion(s + 1);
+    }
+    else 
+    putchar('\n');
+}
