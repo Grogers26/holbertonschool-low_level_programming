43de66ebee517cea0931c1aab27371d28d718f03
George Rogers <115730272+Grogers26@users.noreply.github.com>
2023-06-14 23:12:28 +0000

    looky

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -8,10 +8,14 @@
 int _pow_recursion(int x, int y)
 {
 
-	if (y < 0)
-		return (-1);
-	else if (x = 0)
-		return (0);
+	if (y > 0)
+    {
+        return (x * _pow_recursion(x, y - 1));
+    }
+	else if (y == 0)
+    {
+        return (1);
+    }
 	else
-		return (y * (x - 1));
+		return (-1));
 }
