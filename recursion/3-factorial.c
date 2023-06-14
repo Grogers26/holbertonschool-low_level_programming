0f14d592786a774a53abf0554efccadaef4e74d0
George Rogers <115730272+Grogers26@users.noreply.github.com>
2023-06-14 22:12:11 +0000

    hardest

---
+++ recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * factorial - a function that return the factorial of a given number
+ * @s: given number
+ * Return: n
+ * 
+*/
+int factorial(int n);
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return(1);
+	else 
+		return (n * factorial(n - 1));
+}
