fdf4a384154fbe17d19e690a2450ef7082afac49
George Rogers <115730272+Grogers26@users.noreply.github.com>
2023-06-14 23:05:59 +0000

    wait on me

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion -function that the values of x raised to the power of y
+ * @y: less then 0
+ * Return: -1
+*/
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+		return (-1);
+	else if (x <= 0)
+		return (1);
+	else
+		return (y * (x - 1));
+}
