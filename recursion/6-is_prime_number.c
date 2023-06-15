bcca58553da121c722eb4e14a21a92f9dd84e6fa
George Rogers <115730272+Grogers26@users.noreply.github.com>
2023-06-15 06:41:31 +0000

    mode

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,13 +3,13 @@
 /**
  * is_prime_number - executes prime numbers
  * @n: input to checker
- * Return: always 0 
+ * Return: always 0
  */
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	else if ((n *n/2) > 0)
+	else if ((n * n / 2 ) > 0)
 		return (1);
 	return (0);
 }
