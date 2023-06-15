744642905c1061d0351845901a3ccbe151866fb1
George Rogers <115730272+Grogers26@users.noreply.github.com>
2023-06-15 05:54:33 +0000

    beast

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _sqrt_recursion - returns the natural root of a number
+ * @n: integer
+ * Return: integer - square root, if none returns -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (squareroot(n, (n + 1) / 2));
+}
