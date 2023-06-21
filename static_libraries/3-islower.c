31e3db64d019c4891733685793f8a8498b8d066d
Grogers26 <grogers26@outlook.com>
2023-06-21 00:05:39 -0700

    updated

---
+++ static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
