765600f254e512562cbfc4ac939afa256c0a048f
Grogers26 <grogers26@outlook.com>
2023-06-21 01:30:16 -0700

    boundless

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: array that contain the program arguments
  * Return: 0 - success.
  */
-int main(int argc, char *argv[] _attribute_((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
