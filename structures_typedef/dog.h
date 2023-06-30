823b8b2d27adfcbef924bbfc1939a4cd782a8e30
Grogers26 <grogers26@outlook.com>
2023-06-30 02:21:32 -0700

    like1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -22,7 +22,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+dog_h *new_dog(char *name, float age, char *owner);
+void free_dog(dog_h *d);
 
 #endif
